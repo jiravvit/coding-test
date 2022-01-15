8f6f30cb7bbcd7c6ee8b07dba5d9a050c551b4aa
jiravvit <71068486+jiravvit@users.noreply.github.com>
2022-01-15 20:38:59 +0900

    Delete baekjoon/01_IO_and_Arthimetic directory

--- baekjoon/01_IO_and_Arthimetic/10430.py
+++
@@ -1,9 +0,0 @@
-a,b,c = input().split()
-a = int(a)
-b = int(b)
-c = int(c)
-
-print((a+b)%c)
-print(((a%c)+(b%c))%c)
-print((a*b)%c)
-print(((a%c)*(b%c))%c)
--- baekjoon/01_IO_and_Arthimetic/2588.py
+++
@@ -1,8 +0,0 @@
-a = input()
-b = input()
-a = int(a)
-
-print(a*int(b[-1]))
-print(a*int(b[1]))
-print(a*int(b[0]))
-print(a*int(b))
